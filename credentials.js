a1e914e62ad2eb44e1e3f0384356cf3ec034b59b
Jenna Rajani <jjrajani@gmail.com>
2020-07-07 16:34:27 -0400

    merge no server

---
+++ credentials.js
@@ -0,0 +1,16 @@
+const credentials = {
+  type: "service_account",
+  project_id: "eureka-user-data",
+  private_key_id: "13dffe3f3c7765f756ee1100356d0ea2bdfcb534",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCjeU7DQLXznfGD\nRnaN/fjXscovCsACuX08OnLUx1u1L6mPocMTZb+Nm5qgxN1wBXCcXQIKjPp3lu4i\nC+LLTJo0yvpgFdekWQCk8QigYCCyitSM5b4/VjHgHCUg8I6tIM4+jG0KU4+OUI0I\nCeTZwiTtbloEqjzCf5Al6arK7w6XulEBQR8OJdvzO8Ixx+SOoXKwo2BE0oecP+3M\neWhX94KoTyZv8xHM0iwLgKXhV1T+HiRM4QebIXpiEXP3u84LomARck+K0O3ihgFV\nSYsSYTWdBwIbA6Qbj364wk6MdAFjjsJ5AkDVcCuFiHr00R3YWKBo/itE9CuxDE+U\n7kCOBtVXAgMBAAECggEAC5iqXq1zetGkdUSFWnM+uMZS6l1f25Ge8avPw4pU1hiI\ndbjq5rBcE0aLhIYSk0S/hUgPwgHYLq2VPPMM5pELi10uv6ws/OqXYhv7Ga1YBHJW\nu5v8j++kYBp/A8Qsytk96C/7+DSPRXAvYPeatVvRVMp0NkqBPubfqeDWjcjBhz/C\nM+M2d7NygQr/U5upfrRPf4sT/Ar7I2+/Lg9K2t9NqKB+wdo7WUigLw7sW3xLvOH9\nQ4OehgUm11BJ9YxLLdvGE+5z7fYRtbnYTWtPnY1QthmvTsYu5lWXsnhTXZswMxha\njo7rQ/InudRLgRLV2woNAEa5z6xLk7eeD8Qpnz7pGQKBgQDaJEu86R0nJj/fr3Cx\n/jFTeerdXf+APC8mhr6d0HCwGqA5a3VLCkdhyvJwyVGVFKyMjFQ30eaHzDhb1D9M\nFI5zufLwxcsFIX1a+JKg4AqZA0yLpHVoRdbg6EZrWMw2zZ57ZAeYatyFRek0HK9i\nDpU2L3paQCENlvSdLu8bqew6DQKBgQC/2DMz1th1Hi2WtJ27G1L7J2F6hmgGvx12\n8TumO2rR5YElKlJtvtqqHHtMCJ+JHJ1co5eMIVplz+FItyiMMu/NvdXUsoVneKWj\n7Lt7vhlBDv8NUU3hR0tdVEEyVbXmGldE/oqN07qZTxgutIpqyyUnd5bBFTp2QuKu\nTWzQO5Ln8wKBgQCAG8C++Mg+LaBlZUrxvS6Kr5bW2lUqoi2umoNnevoy8fAWyJzh\nrO+u5tjqk9BtlO6gu+jZv1N7fNu8UjG6JHhkTlf2PrK0GK7dtg2mnOycX+ucNsEc\ns6c1/jMc59FGfgsMu/oZ8OgA/MWv7QiytY27fLaGOhMvP+/4P4/81xX8IQKBgHhu\nS25Of+G9yRajWaUcuRHrHdlhyKn7jY6eTzjCcgND1Le1ArvcJL7HVGtdunDDYtHF\n7JvOUEY7oowYVr70Nu/sLPDyBKb46NqoAOXkZ0wdEuWUVZSHQftbqfXkMmU5wfEQ\n7X+tG6WkLmUbdw0la7SD8UJShjm5MT4EYruxuj4bAoGBALjYf92HkP0FRI5zvQGB\n6sst4t588bhovDo7kygSnNjx0x49aZytQW0+JUvNOuchs/KClLZ2eZJVQMpA2uHc\nJQUpp6IoYZJPDFtvU05js5dLwdGgsWeLkzQqecJpBrrYxp8+ajN6e7Oz4FEzExJa\nchn6TzX65HEOHo5QIfNCZY2t\n-----END PRIVATE KEY-----\n",
+  client_email: "jjrajani@eureka-user-data.iam.gserviceaccount.com",
+  client_id: "105776808887088469679",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/jjrajani%40eureka-user-data.iam.gserviceaccount.com",
+};
+
+export default credentials;
