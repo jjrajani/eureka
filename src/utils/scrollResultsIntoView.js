6d2c935c7fa99a9f6ec83653202eafd61646dc3f
Jenna Rajani <jjrajani@gmail.com>
2020-07-18 17:34:03 -0400

    fix scroll with mobile and tablet

--- src/utils/scrollResultsIntoView.js
+++ src/utils/scrollResultsIntoView.js
@@ -1,15 +1,22 @@
 const scrollResultsIntoView = () => {
   let results = document.getElementById("react-results");
-
+  let wpTopHeader = document.getElementById("top-header");
+  let wpMainHeader = document.getElementById("main-header");
   if (results) {
     var bodyRect = document.body.getBoundingClientRect(),
       elemRect = results.getBoundingClientRect(),
       resultsOffset = elemRect.top - bodyRect.top;
 
+    if (wpTopHeader && wpMainHeader && window.innerWidth > 980) {
+      resultsOffset -= wpTopHeader.clientHeight + wpMainHeader.clientHeight;
+    }
+    console.log("resultsOffset", resultsOffset);
+
     window.scroll({
       top: resultsOffset,
       behavior: "smooth",
     });
+    // results.scrollIntoView({ behavior: "smooth" });
   }
 };
 
